d7d38d39b29f560274639bae8546b18b586987ab
UniBreakfast <unibreakfast1@gmail.com>
2019-02-07 20:20:25 +0200

    questPrice some cases tested

--- questPrice.js
+++ questPrice.js
@@ -1,6 +1,12 @@
-function questPrice(diff, term, fora, etap) {
+function questPrice(diff, term, fora, etap, succs, fails) {
   if (etap) {
-    if (term>=etap*2) diff++
+    if (term<etap/2) diff--
+    else {
+      if (term>=etap*2) diff++
+      if (term>=etap*4) diff++
+      if (term>=etap*8) diff++
+      if (term>=etap*16) diff++
+    }
   }
   return diff * (term - (fora? fora:0));
 }
--- questPrice.test.js
+++ questPrice.test.js
@@ -1,9 +1,10 @@
 const { questPrice } = require('./questPrice')
 
 
-it('multiplies difficulty and term', ()=>
+
+it('multiplies difficulty and term', ()=> {
   expect(questPrice(6, 4)).toBe(24)
-)
+})
 
 it('takes into account fora if given', ()=>
   expect(questPrice(6, 4, 2)).toBe(12)
@@ -13,8 +14,26 @@ it('happy path with etap if specified (and irrelevant)', ()=>
   expect(questPrice(6, 4, 2, 4)).toBe(12)
 )
 
-it('takes into account term much bigger than etap', ()=>
-  expect(questPrice(6, 9, 0, 4)).toBe(63)
+it('takes into account term more than twice bigger than etap',
+  ()=> expect(questPrice(6, 9, 0, 4)).toBe(63)
 )
 
+it('TIA term more than 4 times bigger than etap',
+  ()=> expect(questPrice(2, 18, 0, 4)).toBe(72)
+)
+
+it('TIA term more than 8 times bigger than etap',
+  ()=> expect(questPrice(2, 25, 0, 3)).toBe(125)
+)
+
+it('TIA term more than 2 times smaller than etap',
+  ()=> expect(questPrice(5, 3, 0, 7)).toBe(12)
+)
+
+
+
+
+
 // questPrice(diff, term, fora, etap)
+var date = (new Date()).getMinutes()
+console.log(`~~~~~~~~~~~~~~~~~~~~~~${date}~~~~~~~~~~~~~~~~~~~~~`)
